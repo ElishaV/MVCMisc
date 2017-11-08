4e555fd47a3e564dfd13861a38dd94e72b7de764
elishav <elishav@GEN-DEV40>
2017-11-08 15:33:38 +0200

    Add project files.

---
+++ MVCMiscellaneous/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCMiscellaneous.MvcApplication" Language="C#" %>
---
+++ MVCMiscellaneous/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCMiscellaneous.Startup))]
+namespace MVCMiscellaneous
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
